5f7c57e72d8aed7f85b4745d43c3d4f213deee05
daivy2 <1906444985@qq.com>
2025-09-26 23:27:13 +0800

    3

---
+++ rust_learn/algor_learn/src/bucket_sort.rs
@@ -0,0 +1,33 @@
+pub fn bucket_sort(arr: &[usize]) -> Vec<usize>
+{
+    if arr.is_empty()
+    {
+        return vec![];
+    }
+
+    let max = *arr.iter().max().unwrap();
+    let len = arr.len();
+    let mut buckets = vec![vec![]; len + 1];
+
+    for x in arr
+    {
+        buckets[len * *x / max].push(*x);
+
+    }
+
+    for bucket in buckets.iter_mut()
+    {
+        super::insertion_sort(bucket);
+    }
+
+    let mut result = vec![];
+
+    for bucket in buckets
+    {
+        for x in bucket
+        {
+            result.push(x);
+        }
+    }
+    result
+}
